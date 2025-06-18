f887e11e573c8e74b74e4f6a0408b69513197d68
enoque <eu.enoqueribeiro@gmail.com>
2025-06-18 15:28:17 -0300

    adicionei botão para limpar campos

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
 
 import { ref, computed } from 'vue';
 
-const numero1 = ref(0);
-const numero2 = ref(0);
+const numero1 = ref('');
+const numero2 = ref('');
 const operacao = ref('+');
 
 const resultado = computed(() => {
@@ -21,13 +21,19 @@ const resultado = computed(() => {
   }
 })
 
+function limpar() {
+  numero1.value = '';
+  numero2.value = '';
+  operacao.value = '+';
+}
+
 </script>
 
 <template>
   <div class="container-sm">
 
     <header class="p-5 my-4 text-center">
-      <div class="text-primary fs-3" style="--bs-text-opacity: .7;">Exercício</div>
+      <h2 class="text-primary fs-3 m-0" style="--bs-text-opacity: .7;">Exercício</h2>
       <h1 class="fs-2">Calculadora Vue.JS</h1>
     </header>
 
@@ -41,10 +47,14 @@ const resultado = computed(() => {
     </select>
 
     <input class="form-control" type="number" placeholder="Digite o segundo número" v-model.number="numero2">
+    
+    <button class="btn btn-primary my-3 w-100" @click="limpar">Apagar</button>
 
-    <div class="mt-3">
-      <div class="fs-4 text-primary">Resultado: {{ resultado }}</div>
+    <div class="text-center mt-4">    
+        <p class="text-primary fs-3 m-0">Resultado:</p>
+        <h3 class="fs-1 text-primary p-0 m-0">{{ resultado }}</h3>         
     </div>
+
   </div>
 </template>
 
